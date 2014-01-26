63a946cc1a69dc845f5363cecc67f6dd8c12e2fd
BharathMG <mgbharath@ymail.com>
2014-01-26 07:19:35 +0530

    Minor Changes in Seat Availability

--- src/com/bharathmg/irctc/TrainStatusFragment.java
+++ src/com/bharathmg/irctc/TrainStatusFragment.java
@@ -1,6 +1,8 @@
 package com.bharathmg.irctc;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -46,6 +48,8 @@ public class TrainStatusFragment extends Fragment {
 	private Spinner class_spinner;
 	private Handler handler;
 	private Spinner quota_spinner;
+	private HashMap<String, String> toStationsByCode, fromStationsByCode;
+	private ArrayList<String> classCodes, quotaCodes;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -70,6 +74,54 @@ public class TrainStatusFragment extends Fragment {
 		to_view = (AutoCompleteTextView) rootView.findViewById(R.id.to_text);
 		class_spinner = (Spinner) rootView.findViewById(R.id.spinner1);
 		quota_spinner = (Spinner) rootView.findViewById(R.id.spinner2);
+
+		classCodes = new ArrayList<String>();
+		quotaCodes = new ArrayList<String>();
+		ArrayList<String> classes = new ArrayList<String>();
+		classes.add("First AC");
+		classes.add("Second AC");
+		classes.add("Third AC");
+		classes.add("3 AC Economy");
+		classes.add("AC Chair Car");
+		classes.add("First Class");
+		classes.add("Sleeper Class");
+		classes.add("Second seating");
+
+		classCodes.add("1A");
+		classCodes.add("2A");
+		classCodes.add("3A");
+		classCodes.add("3E");
+		classCodes.add("CC");
+		classCodes.add("FC");
+		classCodes.add("SL");
+		classCodes.add("2S");
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, classes);
+		class_spinner.setAdapter(adapter);
+
+		ArrayList<String> quotas = new ArrayList<String>();
+		quotas.add("Tatkal Quota");
+		quotas.add("Ladies Quota");
+		quotas.add("Defence Quota");
+		quotas.add("Foreign Tourist");
+		quotas.add("Duty Pass Quota");
+		quotas.add("Handicaped Quota");
+		quotas.add("Parliament House Quota");
+		quotas.add("Lower Berth Quota");
+		quotas.add("Yuva Quota");
+		quotas.add("GENERAL QUOTA");
+
+		quotaCodes.add("CK");
+		quotaCodes.add("LD");
+		quotaCodes.add("DF");
+		quotaCodes.add("FT");
+		quotaCodes.add("DP");
+		quotaCodes.add("HP");
+		quotaCodes.add("PH");
+		quotaCodes.add("SS");
+		quotaCodes.add("YU");
+		quotaCodes.add("GN");
+		ArrayAdapter<String> quotas_adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, quotas);
+		quota_spinner.setAdapter(quotas_adapter);
 
 		to_view.setThreshold(1);
 		to_view.addTextChangedListener(new TextWatcher() {
@@ -97,7 +149,6 @@ public class TrainStatusFragment extends Fragment {
 
 							String url = "http://pnrbuddy.com/api/station_by_name/name/" + search_key + "/partial/1/format/json/pbapikey/"
 									+ SecureConstants.public_key + "/pbapisign/" + generator.generateHMAC();
-							System.out.println(url);
 							JsonObjectRequest jsonReq = new JsonObjectRequest(Method.GET, url, null, toViewPopulate(),
 									createMyReqErrorListener());
 							queue.add(jsonReq);
@@ -135,7 +186,6 @@ public class TrainStatusFragment extends Fragment {
 
 							String url = "http://pnrbuddy.com/api/station_by_name/name/" + search_key + "/partial/1/format/json/pbapikey/"
 									+ SecureConstants.public_key + "/pbapisign/" + generator.generateHMAC();
-							System.out.println(url);
 							JsonObjectRequest jsonReq = new JsonObjectRequest(Method.GET, url, null, createMyReqSuccessListener(),
 									createMyReqErrorListener());
 							queue.add(jsonReq);
@@ -154,24 +204,37 @@ public class TrainStatusFragment extends Fragment {
 		String journey_date = journey_date_view.getText().toString();
 		String from_text = from_view.getText().toString();
 		String to_text = to_view.getText().toString();
-		String class_selected = class_spinner.getSelectedItem().toString();
-		String quota_selected = quota_spinner.getSelectedItem().toString();
+		int class_selected = class_spinner.getSelectedItemPosition();
+		int quota_selected = quota_spinner.getSelectedItemPosition();
+		// tnum,fscode,tscode,date,class,quota,format,pbapikey
+		// class,date,format,fscode,pbapikey,quota,tnum,tscode.
+
+		String hmac_input = class_selected + journey_date + "json" + from_text + SecureConstants.public_key + quota_selected + train_number
+				+ to_text;
+		HMACGenerator generator = new HMACGenerator(hmac_input, SecureConstants.private_key);
+		String url = "http://pnrbuddy.com/api/check_avail/tnum/" + train_number + "/fscode/" + fromStationsByCode.get(from_text)
+				+ "/tscode/" + toStationsByCode.get(to_text) + "/date/" + journey_date + "/class/" + classCodes.get(class_selected)
+				+ "/quota/" + quotaCodes.get(quota_selected) + "/format/json/pbapikey/" + SecureConstants.public_key + "/pbapisign/"
+				+ generator.generateHMAC();
 	}
 
 	private Response.Listener<JSONObject> createMyReqSuccessListener() {
 		return new Response.Listener<JSONObject>() {
+
 			@Override
 			public void onResponse(JSONObject response) {
 				try {
 					if (response.getString("response_code").equals("200")) {
+						fromStationsByCode = new HashMap<String, String>();
 						ArrayList<String> stations_list = new ArrayList<String>();
-						System.out.println(response.toString());
 						JSONArray stationsList = response.getJSONArray("stations");
+						fromStationsByCode.clear();
 						for (int i = 0; i < stationsList.length(); i++) {
 							JSONObject stationObj = stationsList.getJSONObject(i);
 							String full_name = stationObj.getString("name");
 							String code_name = stationObj.getString("code");
 							String adapter_value = full_name + " (" + code_name + ")";
+							fromStationsByCode.put(adapter_value, code_name);
 							stations_list.add(adapter_value);
 						}
 						ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
@@ -193,20 +256,24 @@ public class TrainStatusFragment extends Fragment {
 			}
 		};
 	}
+
 	private Response.Listener<JSONObject> toViewPopulate() {
 		return new Response.Listener<JSONObject>() {
+
 			@Override
 			public void onResponse(JSONObject response) {
 				try {
 					if (response.getString("response_code").equals("200")) {
 						ArrayList<String> stations_list = new ArrayList<String>();
-						System.out.println(response.toString());
 						JSONArray stationsList = response.getJSONArray("stations");
+						toStationsByCode = new HashMap<String, String>();
+						toStationsByCode.clear();
 						for (int i = 0; i < stationsList.length(); i++) {
 							JSONObject stationObj = stationsList.getJSONObject(i);
 							String full_name = stationObj.getString("name");
 							String code_name = stationObj.getString("code");
 							String adapter_value = full_name + " (" + code_name + ")";
+							toStationsByCode.put(adapter_value, code_name);
 							stations_list.add(adapter_value);
 						}
 						ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
